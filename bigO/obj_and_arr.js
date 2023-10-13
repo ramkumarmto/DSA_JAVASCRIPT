ef3cca0da61cbbe2da0fbaea6236d82204c8589c
ram kumar <ramkr.mto@gmail.com>
2023-10-13 12:48:18 +0530

    started common patterns to reduce complexity

---
+++ bigO/obj_and_arr.js
@@ -0,0 +1,38 @@
+// built in data structure in javascript
+
+// arrays objects and built in methods 
+
+// Objects
+// unordered key value pair
+// when to use object ?
+// when you have to do fast access,insertion and removal and no order required.
+// insertion  O(1), removal O(1), access O(1) and searching O(n) 
+
+// Object.keys -   O(N)
+
+// Object.values -   O(N)
+
+// Object.entries -   O(N)
+
+// hasOwnProperty -   O(1)
+
+//Arrays
+//ordered list
+// when to use ?
+// don't need order
+// fast insertion, removal and access
+
+// bigO of arrays 
+// Insertion -   It depends.... (it is from beginning or at end) push - O(1) and shift- O(n)
+
+// Removal -   It depends.... (it is from beginning or at end) pop - O(1) and unshift - O(n)
+
+// Searching -   O(N)
+
+// Access -   O(1)
+
+// concat, slice, splice - O(n)
+
+// sort - nlog(n)
+
+// map, filter, forEach, reduce etc having O(n)
---
+++ bigO/problem_solving_pattern.js
@@ -0,0 +1,14 @@
+// some common patterns are 
+// Frequency Counter
+// Multiple Pointers
+// Sliding Window
+// Divide and Conquer
+// Dynamic Programming
+// Greedy Algorithms
+// Backtracking
+// Many more!
+
+// it is not neccessary that all the problems come under this category
+// but maximum of the problem you can solve by this way
+
+//!
