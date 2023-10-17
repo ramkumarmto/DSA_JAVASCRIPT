17a789abe1de288a255dd938d6d4751bb3daf431
ram kumar <ramkr.mto@gmail.com>
2023-10-17 12:38:54 +0530

    searching algorithms started

---
+++ searching_algorithms/basics.js
@@ -0,0 +1,14 @@
+
+// why sorting algorithms are required ?
+
+// it is neccessary that how fast user can search anything from anywhere 
+// like google search engine
+// any data is allready included ( like username)
+// a lot of other example you could think.
+
+
+
+// Implement linear search on arrays
+// Implement binary search on sorted arrays
+// Implement a naive string searching algorithm
+// Implement the KMP string searching algorithm
