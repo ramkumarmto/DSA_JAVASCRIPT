d8b678c2a39c420f349c7fcfd8bd512f09bd185c
ram kumar <ramkr.mto@gmail.com>
2023-10-17 12:26:22 +0530

    slide window method, divide and conquer method to solve problem

--- problem_solving_pattern.js
+++ problem_solving_pattern.js
@@ -173,7 +173,85 @@ function countUniqueValues(arr) {
   return i + 1;
 }
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
-console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
+// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
 // countUniqueValues([]) // 0
 // countUniqueValues([-2,-1,-1,0,1]) // 4
+
+//! sliding window method
+
+// naive method complexity is O(n^2)
+function maxSubarraySum(arr, num) {
+  // if num > arr.length then return null
+  if (num > arr.length) return null;
+
+  // loop through all element
+  let maxValue = 0;
+  for (let i = 0; i < arr.length - num; i++) {
+    let temp = 0;
+    for (let j = i; j < i + num; j++) {
+      // 0
+      temp = temp + arr[j];
+    }
+    if (temp > maxValue) {
+      maxValue = temp;
+    }
+  }
+  console.log("maxVal", maxValue);
+}
+
+// maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2); // 10
+// maxSubarraySum([4,2,1,6],1) // 6
+// maxSubarraySum([4,2,1,6,2],4) // 13
+// maxSubarraySum([], 4); // null
+
+// by sliding window method can decrease the complexity to O(n)
+
+function maxSubarraySum_slide_window(arr, num) {
+  if (arr.length < num) return null;
+
+  let maxval = 0;
+  let tempVal = 0;
+
+  for (let i = 0; i < num; i++) {
+    // we will add elements upto num - 1 index
+    maxval += arr[i];
+  }
+  tempVal = maxval;
+  // then add num index and remove first element and so on
+  for( let i = num; i< arr.length; i++){
+    tempVal = tempVal + arr[i] - arr[i - num]
+    maxval = Math.max(tempVal, maxval)
+  }
+  return maxval;
+}
+
+// console.log(maxSubarraySum_slide_window([1, 2, 5, 2, 8, 1, 5], 4)) // 17
+// console.log(maxSubarraySum_slide_window([1, 2, 5, 2, 8, 1, 5], 2)) // 10
+// console.log(maxSubarraySum_slide_window([4, 2, 1, 6], 1)) // 6
+// console.log(maxSubarraySum_slide_window([4, 2, 1, 6, 2], 4)) // 13
+// console.log(maxSubarraySum_slide_window([], 4)) // null
+
+//! Divide and Conquer
+
+// infirst by naive method (Linear Search) complexity is O(n)
+function search(arr, num) {
+  // first is by javascript inbuilt method
+  // const indexOfNum = arr.indexOf(num)
+  // return indexOfNum;
+
+  // without inbuilt method
+  //if num is present in the array then return index otherwise -1
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+// console.log(search([1,2,3,4,5,6],4)) // 3
+// console.log(search([1,2,3,4,5,6],6)) // 5
+// console.log(search([1,2,3,4,5,6],11)) // -1
+
+// Divide and conquer method we use in Binary search whose complexity is O(logn)
