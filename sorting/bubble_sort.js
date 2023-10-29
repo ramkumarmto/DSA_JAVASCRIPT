a257724fa511dc8a9b565ffff65e9ec15ae26ba8
Ram Kumar Mahto <ramkumar@Rams-MacBook-Air.local>
2023-10-29 11:26:34 +0530

    bubble sort done

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -7,3 +7,82 @@
 // Start an inner loop with a variable called j from the beginning until i - 1
 // If arr[j] is greater than arr[j+1], swap those two values!
 // Return the sorted array
+
+
+//! most basic version
+// function bubbleSort(arr) {
+//     // loop element throughout the array
+//     let count = 0
+
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1; j++) {
+//             count++
+//             console.log(arr, arr[j], arr[j+1], count)
+//             if (arr[j + 1] < arr[j]) {
+//                 // swap
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//         console.log("ONE PASS")
+//     }
+//     console.log(arr)
+
+// }
+
+//! not sorting through allready bubbled element
+// function bubbleSort(arr) {
+//     // loop element throughout the array
+//     let count = 0
+
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - i - 1; j++) {
+//             count++
+//             console.log(arr, arr[j], arr[j+1], count)
+//             if (arr[j + 1] < arr[j]) {
+//                 // swap
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//         console.log("ONE PASS")
+//     }
+//     console.log(arr)
+
+// }
+
+
+
+
+
+//! when elements are nearly sorted
+
+function bubbleSort(arr) {
+    // loop element throughout the array
+    let count = 0
+    let noSwap;
+
+    for (let i = 0; i < arr.length; i++) {
+        noSwap = true;
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            count++
+            console.log(arr, arr[j], arr[j+1], count)
+            if (arr[j + 1] < arr[j]) {
+                // swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwap = false
+            }
+        }
+        if(noSwap) break;
+        console.log("ONE PASS")
+    }
+    console.log(arr)
+
+}
+
+
+console.log(bubbleSort([1, 7, 2, 4, 6]))
